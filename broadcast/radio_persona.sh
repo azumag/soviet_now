270f58ac39aa7b9c4594ae08ff02762026055815
azumag <tsubasa.azumagakito@gmail.com>
2026-03-23 21:25:18 +0900

    eloop Game #9189: score=1876

--- broadcast/radio_persona.sh
+++ broadcast/radio_persona.sh
@@ -163,8 +163,6 @@ _radio_voicevox_speaker_override() {
 	local corner="${1:-}"
 	if [ "$(_radio_host_mode)" = "soren91" ]; then
 		printf '%s' "${SOREN91_VOICEVOX_SPEAKER:-46}"
-	elif [ "$corner" = "rollback" ]; then
-		printf '%s' "${ROLLBACK_VOICEVOX_SPEAKER:-13}"
 	fi
 }
 
